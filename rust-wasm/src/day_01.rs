a1ffdc8ee9ac5c456498b346a157389be5b14966
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-12-01 15:52:28 +0100

    Day 01 part 2

--- rust-wasm/src/day_01.rs
+++ rust-wasm/src/day_01.rs
@@ -1,13 +1,14 @@
-crate::solution!(1, "The first day XXXXX", solve_a);
+crate::solution!(1, "Secret Entrance", solve_a, solve_b);
 
 use nom::{
     IResult, Parser,
     character::complete::{self, line_ending, one_of},
-    multi::{many1, separated_list1},
+    multi::separated_list1,
 };
 
 pub fn solve_a(input: &str) -> u32 {
-    let (_, directions) = parse_a(input).expect("Failed to parse input");
+    let (rest, directions) = parse_a(input).expect("Failed to parse input");
+    assert!(rest.is_empty(), "Unparsed input remaining: {}", rest);
 
     let mut pos: i32 = 50;
     let mut times_at_zero = 0;
@@ -23,6 +24,35 @@ pub fn solve_a(input: &str) -> u32 {
         if pos == 0 {
             times_at_zero += 1;
         }
+    }
+    times_at_zero
+}
+
+pub fn solve_b(input: &str) -> u32 {
+    let (rest, directions) = parse_a(input).expect("Failed to parse input");
+    assert!(rest.is_empty(), "Unparsed input remaining: {}", rest);
+
+    let mut pos: i32 = 50;
+    let mut times_at_zero = 0;
+    for direction in directions {
+        match direction {
+            Direction::Left(dist) => {
+                if dist as i32 >= pos {
+                    times_at_zero += (((dist + 100) as i32 - pos) as f64 / 100.0).floor() as u32;
+                    if pos == 0 {
+                        times_at_zero -= 1;
+                    }
+                }
+                pos -= dist as i32;
+            }
+            Direction::Right(dist) => {
+                if (pos + dist as i32) >= 100 {
+                    times_at_zero += ((pos + dist as i32) as f64 / 100.0).floor() as u32;
+                }
+                pos += dist as i32
+            }
+        }
+        pos = (pos + 10000000) % 100;
     }
     times_at_zero
 }
@@ -52,9 +82,7 @@ fn parse_a(input: &str) -> IResult<&str, Vec<Direction>> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_parse_a() {
-        let input = "L68
+    static INPUT: &str = "L68
 L30
 R48
 L5
@@ -65,24 +93,64 @@ L99
 R14
 L82
 ";
-        let (remaining, directions) = parse_a(input).expect("Failed to parse directions");
+
+    #[test]
+    fn test_parse_a() {
+        let (remaining, directions) = parse_a(INPUT).expect("Failed to parse directions");
         assert_eq!(remaining, "");
         assert_eq!(directions.len(), 10);
     }
 
     #[test]
     fn test_solve_a() {
-        let input = "L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82
-";
-        assert_eq!(solve_a(input), 3);
+        assert_eq!(solve_a(INPUT), 3);
+    }
+
+    #[test]
+    fn test_solve_b() {
+        assert_eq!(solve_b(INPUT), 6);
+    }
+
+    #[test]
+    fn test_wrap_around() {
+        let input = "R1000";
+        assert_eq!(solve_b(input), 10);
+    }
+
+    #[test]
+    fn test_wrap_around2() {
+        let input = "L1000";
+        assert_eq!(solve_b(input), 10);
+    }
+
+    #[test]
+    fn test_wrap_around3() {
+        let input = "R150";
+        assert_eq!(solve_b(input), 2);
+        let input = "R150\nL150";
+        assert_eq!(solve_b(input), 3);
+        let input = "R150\nL200";
+        assert_eq!(solve_b(input), 4);
+        let input = "R150\nL210";
+        assert_eq!(solve_b(input), 4);
+        let input = "R151";
+        assert_eq!(solve_b(input), 2);
+        let input = "R151\nL200";
+        assert_eq!(solve_b(input), 4);
+        let input = "R151\nL220";
+        assert_eq!(solve_b(input), 5);
+    }
+
+    #[test]
+    fn edge_case_zero_start() {
+        let input = "L50";
+        assert_eq!(solve_b(input), 1);
+        let input = "R50";
+        assert_eq!(solve_b(input), 1);
+
+        assert_eq!(solve_b("L50\nL100"), 2);
+        assert_eq!(solve_b("R50\nL100"), 2);
+        assert_eq!(solve_b("L50\nR100"), 2);
+        assert_eq!(solve_b("R50\nR100"), 2);
     }
 }
