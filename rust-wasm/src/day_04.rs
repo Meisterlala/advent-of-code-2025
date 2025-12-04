d0bb0d3b290a665ca4c5f0bac39527a7a3704d69
Meisterlala <6453306+Meisterlala@users.noreply.github.com>
2025-12-04 14:47:04 +0100

    quick and dirty day 4

--- rust-wasm/src/day_04.rs
+++ rust-wasm/src/day_04.rs
@@ -1,4 +1,11 @@
-crate::solution!(4, "Printing Department", r"", &EXAMPLE, solve_a, solve_b);
+crate::solution!(
+    4,
+    "Printing Department",
+    r"This is just a 2D array iteration where i apply a kernel. The current solution is brute force",
+    &EXAMPLE,
+    solve_a,
+    solve_b
+);
 
 static EXAMPLE: &str = "..@@.@@@@.
 @@@.@.@.@@
@@ -78,8 +85,50 @@ pub fn solve_a(input: &str) -> u64 {
 }
 
 pub fn solve_b(input: &str) -> u64 {
-    let (rest, grid) = parse(input).expect("Failed to parse input");
-    todo!()
+    let (_, mut grid) = parse(input).expect("Failed to parse input");
+
+    let mut accessible_rolls = 0u64;
+    let mut changed = true;
+
+    while changed {
+        changed = false;
+        for row_index in 0..grid.len() {
+            for col_index in 0..grid[0].len() {
+                // Not a roll
+                if !grid[row_index][col_index] {
+                    continue;
+                }
+
+                let neighbour_row_start = row_index.saturating_sub(1);
+                let neighbour_col_start = col_index.saturating_sub(1);
+                let neighbour_row_end = (row_index + 1).min(grid.len() - 1);
+                let neighbour_col_end = (col_index + 1).min(grid[col_index].len() - 1);
+
+                let mut neightbour_count = 0;
+                'neighbours: for n_row in neighbour_row_start..=neighbour_row_end {
+                    for n_col in neighbour_col_start..=neighbour_col_end {
+                        if n_row == row_index && n_col == col_index {
+                            continue;
+                        }
+                        if grid[n_row][n_col] {
+                            neightbour_count += 1;
+                        }
+                        // Early exit if already too many neighbours
+                        if neightbour_count > 3 {
+                            break 'neighbours;
+                        }
+                    }
+                }
+                if neightbour_count < 4 {
+                    changed = true;
+                    grid[row_index][col_index] = false;
+                    accessible_rolls += 1;
+                }
+            }
+        }
+    }
+
+    accessible_rolls
 }
 
 #[cfg(test)]
@@ -96,5 +145,10 @@ mod tests {
     #[test]
     fn test_solve_a() {
         assert_eq!(solve_a(EXAMPLE), 13);
+    }
+
+    #[test]
+    fn test_solve_b() {
+        assert_eq!(solve_b(EXAMPLE), 43);
     }
 }
